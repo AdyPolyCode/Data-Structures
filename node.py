9176033e5beff33d292cfc4c3fa0f51bd9b3ad9a
Ady <76212893+AdyPolyCode@users.noreply.github.com>
2021-05-19 11:52:26 -0700

    Add files via upload

---
+++ node.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        '''
+        Node class creater method
+        input value is needed to store a value in it, respectively create a Node
+        '''
+        self.value = value
+        self.nextValue = None
+    
+    def getValue(self):
+        '''
+        Return a single value from the call
+
+        # obj.getValue() -> value
+        '''
+        return self.value
+    
+    def getNextValue(self):
+        '''
+        Calling the next node value
+        
+        ### ### ### ### ###
+        obj1 = Node(value)
+        obj2 = Node(value)
+
+        obj1.setNextValue(obj2)
+        ### ### ### ### ###
+
+        # obj1.getNextValue() -> obj2
+        '''
+        return self.nextValue
+    
+    def setNextValue(self, value):
+        '''
+        Setting a next (node) value to the given node
+
+        obj1 = Node(value)
+        obj2 = Node(value)
+
+        # obj1.setNextValue(obj2)
+        '''
+        self.nextValue = value
